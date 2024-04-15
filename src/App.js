4c282435542c70ade08ee49c83a3a3556b733143
Jbunz <jacob652@aol.com>
2024-04-15 09:28:46 -0700

    adjustments to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import MyNavbar from './Components/TopBar';
-
+import React from 'react';
+import './App.css'; // Import your CSS file
+import MyNavbar from './Components/TopBar'; // Import the MyNavbar component
 
 function App() {
   return (
     <div className="App">
       <MyNavbar /> {/* Render your Navbar component */}
-      <h1>Hello World</h1> {/* Other content */}
+      <div className="content">
+        <h1>Hello World</h1>
+        {/* Other content */}
+      </div>
     </div>
   );
 }
